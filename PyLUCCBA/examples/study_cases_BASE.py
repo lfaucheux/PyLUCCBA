e1d10eb4f1bf300ef486618b9ffeddd32acf204d
Laurent Faucheux <laurent.faucheux@hotmail.fr>
2018-08-23 14:20:15 +0200

    Add files via upload

---
+++ PyLUCCBA/examples/study_cases_BASE.py
@@ -0,0 +1,37 @@
+# -*- coding: utf8 -*-
+
+__authors__ = [
+    "Marion Dupoux <marion.dupoux@u-paris10.fr>",
+    "Laurent Faucheux <faucheux@centre-cired.fr>"
+]
+
+import constant_settings as cs
+import PyLUCCBA as cc
+
+
+objects = {}
+cba = cc.CBACalculator(
+    run_name               = 'Grassland-Cropland_DR=0.03_CP=C_TH=XX',
+    project_horizon        = 20,
+    T_so                   = 20,
+    T_vg_diff              = 1,
+    T_vg_unif              = 20,
+    discount_rate          = .03,
+    co2_prices_scenario    = 'SPC',
+    initial_landuse        = ['FORESTLAND30','improved grassland','annual cropland','degraded grassland'][2],
+    final_landuse          = ['wheat','miscanthus'][1],
+    input_flows_scenario   = ['CRISTANOL','DOE'][1],
+    **cs.other_parameters
+)
+##cba = cc.CBAParametersEndogenizer(cba).ENDOGENOUS_disc_rate_which_equates_NPV_total_unif_co2_flows_TO_NPV_total_diff_co2_flows  
+cba.all_XLSXed
+objects[cba.run_name] = {'summary':cba.summary_args,'object':cba}
+
+print u"""
+*******************************************************************************
+            ╔═╗┬ ┬┌┬┐┌┬┐┌─┐┬─┐┬┌─┐┌─┐                                         *
+            ╚═╗│ │││││││├─┤├┬┘│├┤ └─┐                                         *
+            ╚═╝└─┘┴ ┴┴ ┴┴ ┴┴└─┴└─┘└─┘                                         *
+*******************************************************************************"""
+for run_name, dico in objects.items():
+    print dico['summary']
