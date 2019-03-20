b80c4403836744f81bf66c3ae32dcead700d7639
lfaucheux <laurent.faucheux@hotmail.fr>
2019-03-20 12:54:10 +0100

    Pypi friendly readme file.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import pathlib as pa
 import codecs as cd
 
-package_version = '0.9.04'
+package_version = '0.9.05'
 package_name    = 'PyLUCCBA'
 
 with pa.Path('requirements.txt').open() as requirements:
@@ -35,11 +35,11 @@ setup(
     url          = 'https://github.com/lfaucheux/%s'%package_name,
     download_url = 'https://github.com/lfaucheux/{}/archive/{}.tar.gz'.format(package_name, package_version),
     classifiers  = [
-        'Programming Language :: Python 2.7',
-        'Programming Language :: Python 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Modeling :: Economics',
+        'Topic :: Scientific/Engineering',
     ],
     keywords = [
         'land use change',
